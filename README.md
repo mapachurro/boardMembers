70356466b3ee53c3c4845e135a21a4b4a6160c67
mapachurro <oliver.renwick@gmail.com>
2019-02-12 15:54:41 -0500

    .gitignore fix

--- README.md
+++
@@ -1,2 +0,0 @@
-# boardMembers
-A Heroku-deployed SQL app for updating a webpage showing the board members of an organization.
--- schema.sql
+++
@@ -1,4 +0,0 @@
--- Drops the todolist if it exists currently --
-DROP DATABASE IF EXISTS boardmembers;
--- Creates the "todolist" database --
-CREATE DATABASE boardmembers;
