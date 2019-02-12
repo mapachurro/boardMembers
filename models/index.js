152c692c79e37d07e940c5a787f89e6766d3a12e
mapachurro <oliver.renwick@gmail.com>
2019-02-12 15:59:33 -0500

    trying to fix lowercase

--- models/index.js
+++ models/index.js
@@ -9,9 +9,9 @@ var config = require(__dirname + "/../config/config.json")[env];
 var db = {};
 
 if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+  var sequelize = new secuelizar(process.env[config.use_env_variable]);
 } else {
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
+  var sequelize = new secuelizar(config.database, config.username, config.password, config);
 }
 
 fs
