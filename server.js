d6cdfd97e3583771f18dde736fb522a7020e6448
mapachurro <oliver.renwick@gmail.com>
2019-02-12 21:01:21 -0500

    cleanup on aisle 235

--- server.js
+++ server.js
@@ -27,6 +27,7 @@ require("./routes/api-routes.js")(app);
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
+
 db.sequelize.sync().then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
